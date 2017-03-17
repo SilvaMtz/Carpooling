d1dd418177c16601c6f79bf3233188c37bad2e2b
jorgegil96 <jorgegilcavazos@gmail.com>
2017-03-17 14:15:02 -0600

    Add TabLayout with ViewPager in MainActivity

---
+++ CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/contacts/ContactsFragment.java
@@ -0,0 +1,34 @@
+package itesm.mx.carpoolingtec.contacts;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import itesm.mx.carpoolingtec.R;
+
+public class ContactsFragment extends Fragment {
+
+    public ContactsFragment() {
+        // Required empty public constructor
+    }
+
+    public static ContactsFragment newInstance() {
+        return new ContactsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contacts, container, false);
+    }
+}
