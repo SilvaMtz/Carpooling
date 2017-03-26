b9eef7545801c74e1e4a1e337cc50a720f11a12b
David Martinez <dmtzds@gmail.com>
2017-03-26 13:16:39 -0600

    Fragment Contacts added

---
+++ CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/contacts/ContactsAdapter.java
@@ -0,0 +1,123 @@
+package itesm.mx.carpoolingtec.contacts;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.provider.ContactsContract;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import itesm.mx.carpoolingtec.R;
+import itesm.mx.carpoolingtec.model.Ride;
+import itesm.mx.carpoolingtec.model.User;
+import itesm.mx.carpoolingtec.rides.RideItemListener;
+import itesm.mx.carpoolingtec.rides.RidesAdapter;
+
+/**
+ * Created by DavidMartinez on 3/26/17.
+ */
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder>{
+
+
+
+    private Context context;
+    private List<User> contacts;
+    private ContactItemListener listener;
+
+    public ContactsAdapter(Context context, List<User> contacts, ContactItemListener listener) {
+        this.context = context;
+        this.contacts = contacts;
+        this.listener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.contact_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        final User contact = contacts.get(position);
+
+        Picasso.with(context)
+                .load(contact.getFoto())
+                .into(holder.ivPicture, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Bitmap imageBitmap = ((BitmapDrawable) holder.ivPicture.getDrawable())
+                                .getBitmap();
+                        RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory
+                                .create(context.getResources(), imageBitmap);
+                        drawable.setCircular(true);
+                        drawable.setCornerRadius(Math.max(imageBitmap.getWidth(),
+                                imageBitmap.getHeight()) / 2.0f);
+                        holder.ivPicture.setImageDrawable(drawable);
+                    }
+
+                    @Override
+                    public void onError() {
+                        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.gates);
+                        RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory
+                                .create(context.getResources(), bitmap);
+                        drawable.setCircular(true);
+                        drawable.setCornerRadius(Math.max(bitmap.getWidth(),
+                                bitmap.getHeight()) / 2.0f);
+
+                        holder.ivPicture.setImageDrawable(drawable);
+                    }
+                });
+
+        holder.tvName.setText(contact.getNombre());
+        holder.rlContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onContactClick(contact);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return null != contacts ? contacts.size() : 0;
+    }
+
+    public void setData(List<User> data) {
+        contacts.clear();
+        contacts.addAll(data);
+        notifyDataSetChanged();
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.contactLayout) RelativeLayout rlContainer;
+        @BindView(R.id.image_user) ImageView ivPicture;
+        @BindView(R.id.text_name) TextView tvName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/contacts/ContactsFragment.java
+++ CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/contacts/ContactsFragment.java
@@ -4,13 +4,31 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
-import itesm.mx.carpoolingtec.R;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ContactsFragment extends Fragment {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import itesm.mx.carpoolingtec.R;
+import itesm.mx.carpoolingtec.model.User;
+
+
+public class ContactsFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, ContactItemListener {
+
+    @BindView(R.id.swipeRefreshLayout) SwipeRefreshLayout swipeRefreshLayout;
+    @BindView(R.id.rv_contacts) RecyclerView recyclerView;
+
+    private ContactsAdapter contactsAdapter;
+    private Unbinder unbinder;
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -23,12 +41,52 @@ public class ContactsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu){
+        menu.findItem(R.id.action_sort).setVisible(true);
+        super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        contactsAdapter = new ContactsAdapter(getActivity(), getDummyContacts(), this);
+        recyclerView.setAdapter(contactsAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    private List<User> getDummyContacts() {
+        List<User> contacts = new ArrayList<>();
+        contacts.add(new User("Mariel Palacios","http://orig04.deviantart.net/aded/f/2013/066/c/2/profile_picture_by_naivety_stock-d5x8lbn.jpg", "Valle Primavera"));
+        contacts.add(new User("Kenan Zertuche","http://orig10.deviantart.net/b1f3/f/2011/258/1/8/profile_picture_by_ff_stock-d49yyse.jpg", "Narvarte"));
+        contacts.add(new User("Kel Zertuche","http://skateparkoftampa.com/spot/headshots/2585.jpg", "Lomas de Rosales"));
+
+        return contacts;
+    }
+
+    @Override
+    public void onContactClick(User contact) {
+
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 }
