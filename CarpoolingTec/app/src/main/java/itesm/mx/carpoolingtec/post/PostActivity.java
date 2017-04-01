8d8414c7a8345aea4143507036b0330e5710dedf
jorgegil96 <jorgegilcavazos@gmail.com>
2017-04-01 15:14:21 -0600

    Use Places API to select origin/destination. Close #33

--- CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/post/PostActivity.java
+++ CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/post/PostActivity.java
@@ -3,6 +3,7 @@ package itesm.mx.carpoolingtec.post;
 import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +11,20 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,12 +39,18 @@ import itesm.mx.carpoolingtec.model.firebase.User;
 import itesm.mx.carpoolingtec.util.schedulers.BaseSchedulerProvider;
 import itesm.mx.carpoolingtec.util.schedulers.SchedulerProvider;
 
-public class PostActivity extends AppCompatActivity implements PostView {
+public class PostActivity extends AppCompatActivity implements PostView, PlaceSelectionListener,
+        GoogleApiClient.OnConnectionFailedListener, AdapterView.OnItemSelectedListener {
 
     private static final String TAG = "PostActivity";
+    private static final String ORIGEN = "Origen";
+    private static final String DESTINO = "Destino";
 
     @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.tipoSpinner) Spinner spinner;
 
+    private PlaceAutocompleteFragment autocompleteFragment;
+    private GoogleApiClient googleApiClient;
     private PostPresenter presenter;
 
     @Override
@@ -47,12 +66,24 @@ public class PostActivity extends AppCompatActivity implements PostView {
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
         }
 
-        Spinner spinner = (Spinner) findViewById(R.id.tipoSpinner);
+        googleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.viaje_spinner, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        autocompleteFragment.setBoundsBias(new LatLngBounds(
+                new LatLng(25.596967, 100.488252),
+                new LatLng(25.816600, 100.098237)
+        ));
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+        // Set default hint to ORIGEN
+        autocompleteFragment.setHint(ORIGEN);
+
+        spinner.setOnItemSelectedListener(this);
 
         User user = new User("A00513176", "Juan Perez", "http://skateparkoftampa.com/spot/headshots/2585.jpg");
         Ride ride = new Ride("FROM_TEC", "11:30 am", "Miercoles", 12345678.0, 87654321.0);
@@ -86,5 +117,37 @@ public class PostActivity extends AppCompatActivity implements PostView {
     protected void onDestroy() {
         super.onDestroy();
         presenter.stop();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onPlaceSelected(Place place) {
+        // TODO: Get info about the selected place.
+        Log.i(TAG, "Place: " + place.getName());
+    }
+
+    @Override
+    public void onError(Status status) {
+        // TODO: Handle the error.
+        Log.i(TAG, "An error occurred: " + status);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.d(TAG, parent.getItemAtPosition(position).toString() + " selected");
+        if (position == 0) {
+            autocompleteFragment.setHint(ORIGEN);
+        } else {
+            autocompleteFragment.setHint(DESTINO);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
