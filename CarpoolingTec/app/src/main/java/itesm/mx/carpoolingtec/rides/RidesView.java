3b879bed830c079e5e49ce1a0525424b9b51ba66
jorgegil96 <jorgegilcavazos@gmail.com>
2017-05-02 18:52:13 -0500

    Change ride adapter to firebase adapter

--- CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/rides/RidesView.java
+++ CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/rides/RidesView.java
@@ -7,18 +7,6 @@ import itesm.mx.carpoolingtec.model.firebase.UserRide;
 
 public interface RidesView {
 
-    void setLoadingIndicator(boolean active);
-
-    void showUserRides();
-
-    void hideUserRides();
-
-    void addUserRide(UserRide userRide);
-
-    void clearUserRides();
-
-    void showErrorLoadingRidesToast();
-
     void openUserRideDetails(User user,
                              List<String> ridesMonday,
                              List<String> ridesTuesday,
---
+++ CarpoolingTec/app/src/main/java/itesm/mx/carpoolingtec/rides/UserRideHolder.java
@@ -0,0 +1,27 @@
+package itesm.mx.carpoolingtec.rides;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import itesm.mx.carpoolingtec.R;
+
+public class UserRideHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.container)
+    RelativeLayout rlContainer;
+    @BindView(R.id.image_user)
+    ImageView ivPicture;
+    @BindView(R.id.text_name)
+    TextView tvName;
+    @BindView(R.id.text_description) TextView tvDescription;
+
+    public UserRideHolder(View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+}
